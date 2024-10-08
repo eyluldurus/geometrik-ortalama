c59e7aaff63e177e8454105ee88f99b59a2e470f
eyluldurus <eyll.durus@gmail.com>
2024-10-08 18:31:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//string ,int ,float,double,bool;
+
+
+
+using System.ComponentModel;
+
+
+int sayi1;
+int sayi2;
+double sonuc;
+
+Console.WriteLine("Sayı biri giriniz.");
+sayi1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Sayı ikiyi giriniz.");
+sayi2 = int.Parse(Console.ReadLine());
+
+sonuc = Math.Sqrt(sayi1 * sayi2);
+Console.WriteLine($"Sonuç : {sonuc}");
+
+
